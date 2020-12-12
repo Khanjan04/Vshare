03d2b3373cdc2467c2e9579d0d41fc247fd52dde
Khanjan <khanjanvarma9@gmail.com>
2020-12-12 19:44:53 +0530

    Few files added

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Vshare
-A website for VIT students. A website where they can find updates on notices and announcements. Also notes related to different subjects.
+
+**_What is Vshare?_**
+- A MERN Web App where teachers and students can connect with each other. 
+- A one stop destination for students and teachers. 
+- Here teachers can share notes with students and can make announcements🧑‍💻.
+
+**_About:_**
+- Vshare is a simple solution for VIT teachers and students to come near. With simple steps teacher can upload notes and students can use it.
+**_Tech Stack:_**
+- React.js
+- Express.js
+- Node.js
+- MongoDB
+- Bootstrap
---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var app = express();
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var path = require("path");
+require("dotenv").config();
+
+// SOME COMMON MIDDLEWARES
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static("./client/build"));
+
+// DATABASE CONNECTION
+
+mongoose.connect(
+  process.env.MONGODB_URL || process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (error) {
+    if (error) console.log(error);
+    // Handle failed connection
+    else
+      console.log(
+        "Database connected. Connection State -> " +
+          mongoose.connection.readyState
+      );
+  }
+);
+
+// MAIN ROUTES
+app.use("/college/teacher", require("./Routes/teacher"));
+
+// SERVE STATIC ASSETS WHEN WE ARE IN PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+var port = process.env.PORT || 8000;
+app.listen(port, (req, res) => {
+  console.log(`Woah! App is running on port ${port}`);
+});
