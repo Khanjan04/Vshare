dce5c6be14057cbf005faf7e862e3886a0e84e27
Khanjan <khanjanvarma9@gmail.com>
2020-12-13 14:57:07 +0530

    changes layout

--- frontend/src/Components/Landing.js
+++ frontend/src/Components/Landing.js
@@ -5,15 +5,17 @@ import laptop from "../Images/laptop.jpg";
 const Landing = () => {
   return (
     <div className="hero">
-      <div className="hero-image">
-        <img src={laptop} />{" "}
-      </div>
-      <div className="hero-text">
-        <h2>Vshare</h2>
-        <Typography variant="h5" color="textSecondary" component="p">
-            is a one stop destination for VIT students and teachers. Here
-          teachers can share notes with students and can make announcements.
-        </Typography>
+      <div className="panel">
+        <div className="hero-image">
+          <img src={laptop} />{" "}
+        </div>
+        <div className="hero-text">
+          <h2>Vshare</h2>
+          <Typography variant="h5" color="textSecondary" component="p">
+              is a one stop destination for VIT students and teachers. Here
+            teachers can share notes with students and can make announcements.
+          </Typography>
+        </div>
       </div>
       <div>
       <>
