6a14d69c160c11c5041a20b8dfaddf861abbaf7c
Khanjan <khanjanvarma9@gmail.com>
2020-12-13 19:19:40 +0530

    events pictures added

--- frontend/src/Components/Cards.js
+++ frontend/src/Components/Cards.js
@@ -22,45 +22,25 @@ function Cards() {
               path='/services'
             />
             <CardItem
-              src='/images/img-2.jpg'
-              text='Cleaning camp on parvati hills on Sunday'
-              label='SWD Club'
-              path='/services'
-            />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
               src='/images/img-3.jpg'
               text='Cyber Security in Web and Android Technologies'
               label='IEEE'
               path='/services'
             />
+          </ul>
+          <ul className='cards__items'>
             <CardItem
               src='/images/img-4.jpg'
-              text='Inter-College Football championship in COPE PUNE'
+              text='Inter-College Football championship in COEP PUNE'
               label='Sports'
               path='/products'
             />
             <CardItem
-              src='/images/img-8.jpg'
+              src='/images/img-5.jpg'
               text='Get the basics of HTML, CSS and Javascript for Web Development'
               label='DSC'
               path='/sign-up'
             />
-          </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='/images/img-9.jpg'
-              text='Raspberry-pie workshop on robotics  and bluetooth integration with arduino'
-              label='TRF'
-              path='/services'
-            />
-            <CardItem
-              src='/images/img-2.jpg'
-              text='Cleaning camp on parvati hills on Sunday'
-              label='SWD Club'
-              path='/services'
-            />
             <CardItem
               src='/images/img-2.jpg'
               text='Cleaning camp on parvati hills on Sunday'
@@ -82,7 +62,7 @@ function Cards() {
               path='/products'
             />
             <CardItem
-              src='/images/img-8.jpg'
+              src='/images/img-5.jpg'
               text='Get the basics of HTML, CSS and Javascript for Web Development'
               label='DSC'
               path='/sign-up'
